1dda6228765f95da0c38f0283e334a1bed6d5f75
GT-ZhangAcer <1029550448@qq.com>
2020-01-31 22:30:30 +0800

    NN Demo

--- server-python/models/sampleNN.py
+++ server-python/models/sampleNN.py
@@ -3,3 +3,21 @@
 # Copyright belongs to the author.
 # Please indicate the source for reprinting.
 
+import paddle.fluid as fluid
+
+
+class SampleNN:
+    def __init__(self, mode: int = 1):
+        self.mode = mode
+
+    def main_network(self, lod_data):
+        emb = fluid.embedding(input=lod_data, size=[512, 100], is_sparse=True)
+        print(emb.shape)
+        x = fluid.layers.fc(input=emb, size=512 * 3)
+        hidden = fluid.layers.dynamic_gru(input=x, size=512)
+        print(hidden.shape)
+
+
+data = fluid.data(name="test", shape=[-1], dtype="int64", lod_level=1)
+SampleNN().main_network(data)
+
