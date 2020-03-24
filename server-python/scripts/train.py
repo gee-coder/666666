f0111172e57f75e6c74ea6a2c28c6dacf23fc295
GT-ZhangAcer <1029550448@qq.com>
2020-03-24 14:35:47 +0800

    Release V5.2 change print color.

--- server-python/scripts/train.py
+++ server-python/scripts/train.py
@@ -5,18 +5,14 @@
 
 import os
 import time
-
-import paddle.fluid as fluid
-
-import numpy as np
-from scripts.CSNN import CSNN
-from scripts.preprocess import reader
-from scripts.os_tool import GLog
-
 import logging as log
 
-log.basicConfig(level=log.DEBUG,
-                format='%(asctime)s: %(message)s')
+import paddle.fluid as fluid
+import numpy as np
+
+from scripts.CSNN import CSNN
+from scripts.preprocess import reader
+from scripts.os_tool import GLog, req_time_id
 
 # config
 USE_CUDA = False
@@ -29,6 +25,10 @@ config = {
     "BOUNDARIES": [30, 200, 500, 1000, 3000],
     "LR_STEPS": [0.01, 0.001, 0.0001, 0.00001, 0.000005, 0.000001]
 }
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s: %(message)s',
+                filename=os.path.join(ROOT_PATH, "config/" + req_time_id()))
 
 # environment
 place = fluid.CUDAPlace(0) if USE_CUDA else fluid.CPUPlace()
@@ -84,7 +84,6 @@ val_feeder = fluid.DataFeeder(
 config["val_acc"] = None
 config["seed"] = None
 log1 = GLog(gpack_path=ROOT_PATH + "/config", item_heads=config, file_name="train_log2", new_file=True)
-log2 = GLog(gpack_path=ROOT_PATH + "/config", item_heads={"loss": None, "acc": None}, new_file=True)
 FIRST_FLAG = False
 DATA_NUM = 0
 
@@ -119,9 +118,11 @@ def controller_process(program, data_reader, feeder):
     acc2 = sum(acc2) / len(acc2)
     if FIRST_FLAG is False:
         DATA_NUM = len(infos["loss"]) * config["BATCH_SIZE"] / 0.8
-        print("|TRAIN_DATA_NUM|\t|", DATA_NUM)
+        log.info("|TRAIN_DATA_NUM|\t|" + str(DATA_NUM))
         FIRST_FLAG = True
-    return "\t|loss:{:.4f}".format(loss_info), "\t|Error Rate:{:.4f} %".format(error_rate * 100), acc1, acc2
+    msg = "\t|loss:{:.4f}".format(loss_info) + "\t|Error Rate:{:.4f} %".format(
+        error_rate * 100) + "\t|K1:{:.2f}%".format(acc1 * 100) + "|K2:{:.2f}%".format(acc2 * 100)
+    return msg, acc2
 
 
 val_acc = 0
@@ -140,20 +141,18 @@ def if_exist(var):
 
 fluid.io.load_vars(controller, os.path.join(ROOT_PATH, "ERNIE/params"),
                    main_program=train_program, predicate=if_exist)
-log.info(msg="读取" + str(len(load_params_num)) + "组参数，若参数量低于100，请检查配置文件")
+log.info(msg="\033[1;31m 读取" + str(len(load_params_num)) + "组参数，若参数量低于100，请检查配置文件 \033[0m!")
 
 for epoch in range(config["EPOCHE_NUM"]):
-    train_info = controller_process(train_program, train_reader, train_feeder)
+    train_info, _ = controller_process(train_program, train_reader, train_feeder)
     start_time = time.time()
-    val_info = controller_process(val_program, val_reader, val_feeder)
+    val_info, val_acc = controller_process(val_program, val_reader, val_feeder)
     avg_sample = (time.time() - start_time) / (DATA_NUM * 0.2)
-    log2.write_message("|TRAIN|\t|Epoch:", epoch, train_info[0], train_info[1], "|\t|VAL|", val_info[1])
-    print("|TRAIN|\t|Epoch:", epoch, train_info[0], train_info[1], "|\t|VAL|", val_info[1],
-          "\t|K1:{:.2f}%".format(val_info[2] * 100), "|K2:{:.2f}%".format(val_info[3] * 100),
-          "\t|SAMPLE TIME:{:.6f}/s".format(avg_sample))
-    val_acc += val_info[2]
-    if max_val_acc < val_info[2]:
-        max_val_acc = val_info[2]
+    log.info("\033[1;35m |EPOCH:" + str(epoch) + "\t|SAMPLE TIME:{:.6f}/s".format(avg_sample)+"\033[0m!")
+    log.info("\033[1;34m |TRAIN:" + train_info + "\033[0m!")
+    log.info("\033[1;34m |VAL:" + val_info + "\033[0m!")
+    if max_val_acc < val_acc:
+        max_val_acc = val_acc
         fluid.io.save_persistables(controller, "./save_params", main_program=train_program)
 
 config["seed"] = train_program.random_seed
--- server-python/test4.py
+++ server-python/test4.py
@@ -3,16 +3,6 @@
 # Copyright belongs to the author.
 # Please indicate the source for reprinting.
 
-import paddlehub as hub
+import logging as log
 
-# Load ernie pretrained model
-module = hub.Module(name="ernie_tiny")
-inputs, outputs, program = module.context(trainable=True, max_seq_len=128)
-
-# Must feed all the tensor of ernie's module need
-input_ids = inputs["input_ids"]
-position_ids = inputs["position_ids"]
-segment_ids = inputs["segment_ids"]
-input_mask = inputs["input_mask"]
-
-print(input_ids)
+print('This is a \033[1;35m test')
